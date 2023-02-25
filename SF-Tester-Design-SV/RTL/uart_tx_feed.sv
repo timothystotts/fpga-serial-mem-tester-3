0f082587e3f90f803718e2c9126e978cf0b49f7d
Timothy Stotts <timothystotts08@gmail.com>
2023-02-24 20:18:14 -0500

    The entity and module uart_tx_feed were updated for comments and/or whitespace.

--- SF-Tester-Design-SV/RTL/uart_tx_feed.sv
+++ SF-Tester-Design-SV/RTL/uart_tx_feed.sv
@@ -1,7 +1,7 @@
 /*------------------------------------------------------------------------------
 -- MIT License
 --
--- Copyright (c) 2020-2022 Timothy Stotts
+-- Copyright (c) 2020-2023 Timothy Stotts
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
@@ -30,32 +30,33 @@
 //Recursive Moore Machine-------------------------------------------------------
 //Part 1: Module header:--------------------------------------------------------
 module uart_tx_feed
-	#(parameter
-		parm_ascii_line_length = 35
-		)
-	(
-		// system clock and reset
-		input logic i_clk_40mhz,
-		input logic i_rst_40mhz,
-		// data and valid pulse output to the UART TX
-		output logic [7:0] o_tx_data,
-		output logic o_tx_valid,
-		// the TX Ready input from the UART TX
-		input logic i_tx_ready,
-		input logic i_tx_go,
-		// data captured as next 35 character line to transmit
-		input logic [(parm_ascii_line_length*8-1):0] i_dat_ascii_line
-	);
+    #(parameter
+        parm_ascii_line_length = 35
+        )
+    (
+        // system clock and reset
+        input logic i_clk_40mhz,
+        input logic i_rst_40mhz,
+        // data and valid pulse output to the UART TX
+        output logic [7:0] o_tx_data,
+        output logic o_tx_valid,
+        // the TX Ready input from the UART TX
+        input logic i_tx_ready,
+        // system pulse to start transmit of a new line
+        input logic i_tx_go,
+        // data captured as next 35 character line to transmit
+        input logic [(parm_ascii_line_length*8-1):0] i_dat_ascii_line
+    );
 
 //Part 2: Declarations----------------------------------------------------------
 timeunit 1ns;
 timeprecision 1ps;
 
-/* UART TX update FSM state declarations */
+// UART TX update FSM state declarations
 `define c_uarttx_feed_fsm_bits 2
 
 typedef enum logic [(`c_uarttx_feed_fsm_bits - 1):0] {
-	ST_UARTFEED_IDLE, ST_UARTFEED_CAPT, ST_UARTFEED_DATA, ST_UARTFEED_WAIT
+    ST_UARTFEED_IDLE, ST_UARTFEED_CAPT, ST_UARTFEED_DATA, ST_UARTFEED_WAIT
 } t_uartfeed_state;
 
 // UART feed FSM state register
@@ -66,10 +67,10 @@ t_uartfeed_state s_uartfeed_nx_state;
 localparam [5:0] c_uart_k_preset = parm_ascii_line_length;
 
 // preset values on reset
-localparam [(parm_ascii_line_length*8-1):0] c_line_of_spaces = 
-	280'h2020202020202020202020202020202020202020202020202020202020202020200D0A;
+localparam [(parm_ascii_line_length*8-1):0] c_line_of_spaces =
+    280'h2020202020202020202020202020202020202020202020202020202020202020200D0A;
 
-/* UART TX signals for UART TX update FSM */
+// UART TX signals for UART TX update FSM
 logic [5:0] s_uart_k_val;
 logic [5:0] s_uart_k_aux;
 logic [(parm_ascii_line_length*8-1):0] s_uart_line_val;
@@ -86,74 +87,74 @@ logic [(parm_ascii_line_length*8-1):0] s_uart_line_aux;
 // and auxiliary line data register LINE.
 always_ff @(posedge i_clk_40mhz)
 begin: p_uartfeed_fsm_state_aux
-	if (i_rst_40mhz) begin
-		s_uartfeed_pr_state <= ST_UARTFEED_IDLE;
-		s_uart_k_aux <= 0;
-		s_uart_line_aux <= c_line_of_spaces;
-	end
-	else begin : if_fsm_state_and_storage
-		s_uartfeed_pr_state <= s_uartfeed_nx_state;
-		s_uart_k_aux <= s_uart_k_val;
-		s_uart_line_aux <= s_uart_line_val;
-	end : if_fsm_state_and_storage
+    if (i_rst_40mhz) begin
+        s_uartfeed_pr_state <= ST_UARTFEED_IDLE;
+        s_uart_k_aux <= 0;
+        s_uart_line_aux <= c_line_of_spaces;
+    end
+    else begin : if_fsm_state_and_storage
+        s_uartfeed_pr_state <= s_uartfeed_nx_state;
+        s_uart_k_aux <= s_uart_k_val;
+        s_uart_line_aux <= s_uart_line_val;
+    end : if_fsm_state_and_storage
 end : p_uartfeed_fsm_state_aux
 
-/* UART TX machine, combinatorial next state and auxiliary counting register, and
-   auxiliary 34 8-bit character line register. */
+// UART TX machine, combinatorial next state and auxiliary counting register, and
+// auxiliary 34 8-bit character line register.
 always_comb
 begin: p_uartfeed_fsm_nx_out
-	case (s_uartfeed_pr_state)
-		ST_UARTFEED_CAPT: begin
-			// Capture the input ASCII line and the index K.
-			// The value of \ref i_tx_ready is also checked as to
-			// not overflow the UART TX buffer. Once TX is ready,
-			// begin the enqueue of outgoing data. TX Ready is presumed to
-			// indicate that the TX FIFO is below the threshold of almost
-			// full and that enqueueing the full line will not overflow
-			// the TX FIFO.
-			o_tx_data = 8'h00;
-			o_tx_valid = 1'b0;
-			s_uart_k_val = c_uart_k_preset;
-			s_uart_line_val = i_dat_ascii_line;
+    case (s_uartfeed_pr_state)
+        ST_UARTFEED_CAPT: begin
+            // Capture the input ASCII line and the index K.
+            // The value of \ref i_tx_ready is also checked as to
+            // not overflow the UART TX buffer. Once TX is ready,
+            // begin the enqueue of outgoing data. TX Ready is presumed to
+            // indicate that the TX FIFO is below the threshold of almost
+            // full and that enqueueing the full line will not overflow
+            // the TX FIFO.
+            o_tx_data = '0;
+            o_tx_valid = 1'b0;
+            s_uart_k_val = c_uart_k_preset;
+            s_uart_line_val = i_dat_ascii_line;
 
-			if (i_tx_ready) s_uartfeed_nx_state = ST_UARTFEED_DATA;
-			else s_uartfeed_nx_state = ST_UARTFEED_CAPT;
-		end
-		ST_UARTFEED_DATA: begin
-			/* Enqueue the \ref c_uart_k_preset count of bytes from register
-			   \ref s_uart_line_aux. Then transition to the WAIT state.
-			   To accomplish this, s_uart_line_aux is shifted left, one byte
-			   at-a-time. */
-			o_tx_data = s_uart_line_aux[((8*c_uart_k_preset)-1)-:8];
-			o_tx_valid = 1'b1;
-			s_uart_k_val = s_uart_k_aux - 1;
-			s_uart_line_val = {s_uart_line_aux[(8*(c_uart_k_preset-1)-1)-:(8*(c_uart_k_preset-1))],8'h00};
+            if (i_tx_ready) s_uartfeed_nx_state = ST_UARTFEED_DATA;
+            else s_uartfeed_nx_state = ST_UARTFEED_CAPT;
+        end
+        ST_UARTFEED_DATA: begin
+            // Enqueue the \ref c_uart_k_preset count of bytes from register
+            // \ref s_uart_line_aux. Then transition to the WAIT state.
+            // To accomplish this, s_uart_line_aux is shifted left, one byte
+            // at-a-time.
+            o_tx_data = s_uart_line_aux[((8*c_uart_k_preset)-1)-:8];
+            o_tx_valid = 1'b1;
+            s_uart_k_val = s_uart_k_aux - 1;
+            s_uart_line_val = {s_uart_line_aux[(8*(c_uart_k_preset-1)-1)-:(8*(c_uart_k_preset-1))],8'h00};
 
-			if (s_uart_k_aux == 1) s_uartfeed_nx_state = ST_UARTFEED_WAIT;
-			else s_uartfeed_nx_state = ST_UARTFEED_DATA;
-		end
-		ST_UARTFEED_WAIT: begin
-			/* Wait for the \ref i_tx_go pulse to be low, and then
-			   transition to the IDLE state. */
-			o_tx_data = 8'h00;
-			o_tx_valid = 1'b0;
-			s_uart_k_val = s_uart_k_aux;
-			s_uart_line_val = s_uart_line_aux;
+            if (s_uart_k_aux == 1) s_uartfeed_nx_state = ST_UARTFEED_WAIT;
+            else s_uartfeed_nx_state = ST_UARTFEED_DATA;
+        end
+        ST_UARTFEED_WAIT: begin
+            // Wait for the \ref i_tx_go pulse to be low, and then
+            // transition to the IDLE state.
+            o_tx_data = '0;
+            o_tx_valid = 1'b0;
+            s_uart_k_val = s_uart_k_aux;
+            s_uart_line_val = s_uart_line_aux;
 
-			if (! i_tx_go) s_uartfeed_nx_state = ST_UARTFEED_IDLE;
-			else s_uartfeed_nx_state = ST_UARTFEED_WAIT;
-		end
-		default: begin // ST_UARTFEED_IDLE
-			/* IDLE the FSM while waiting for a pulse on \ref i_tx_go . */
-			o_tx_data = 8'h00;
-			o_tx_valid = 1'b0;
-			s_uart_k_val = s_uart_k_aux;
-			s_uart_line_val = s_uart_line_aux;
+            if (! i_tx_go) s_uartfeed_nx_state = ST_UARTFEED_IDLE;
+            else s_uartfeed_nx_state = ST_UARTFEED_WAIT;
+        end
+        default: begin // ST_UARTFEED_IDLE
+            // IDLE the FSM while waiting for a pulse on \ref i_tx_go .
+            o_tx_data = '0;
+            o_tx_valid = 1'b0;
+            s_uart_k_val = s_uart_k_aux;
+            s_uart_line_val = s_uart_line_aux;
 
-			if (i_tx_go) s_uartfeed_nx_state = ST_UARTFEED_CAPT;
-			else s_uartfeed_nx_state = ST_UARTFEED_IDLE;
-		end
-	endcase
+            if (i_tx_go) s_uartfeed_nx_state = ST_UARTFEED_CAPT;
+            else s_uartfeed_nx_state = ST_UARTFEED_IDLE;
+        end
+    endcase
 end : p_uartfeed_fsm_nx_out
 
 endmodule : uart_tx_feed
--- SF-Tester-Design-VHDL/RTL/uart_tx_feed.vhdl
+++ SF-Tester-Design-VHDL/RTL/uart_tx_feed.vhdl
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------
 -- MIT License
 --
--- Copyright (c) 2020 Timothy Stotts
+-- Copyright (c) 2020,2023 Timothy Stotts
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
@@ -32,143 +32,143 @@ use ieee.numeric_std.all;
 library work;
 --------------------------------------------------------------------------------
 entity uart_tx_feed is
-	generic(
-		-- the ASCII line length
-		parm_ascii_line_length : natural := 35
-	);
-	port(
-		-- system clock and reset
-		i_clk_40mhz      : in  std_logic;
-		i_rst_40mhz      : in  std_logic;
-		-- data and valid pulse output to the UART TX
-		o_tx_data        : out std_logic_vector(7 downto 0);
-		o_tx_valid       : out std_logic;
-		-- the TX Ready input from the UART TX
-		i_tx_ready       : in  std_logic;
-		-- system pulse to start transmit of a new line
-		i_tx_go          : in  std_logic;
-		-- data captured as next 35 character line to transmit
-		i_dat_ascii_line : in  std_logic_vector((parm_ascii_line_length*8-1) downto 0)
-	);
+    generic(
+        -- the ASCII line length
+        parm_ascii_line_length : natural := 35
+    );
+    port(
+        -- system clock and reset
+        i_clk_40mhz      : in  std_logic;
+        i_rst_40mhz      : in  std_logic;
+        -- data and valid pulse output to the UART TX
+        o_tx_data        : out std_logic_vector(7 downto 0);
+        o_tx_valid       : out std_logic;
+        -- the TX Ready input from the UART TX
+        i_tx_ready       : in  std_logic;
+        -- system pulse to start transmit of a new line
+        i_tx_go          : in  std_logic;
+        -- data captured as next 35 character line to transmit
+        i_dat_ascii_line : in  std_logic_vector((parm_ascii_line_length*8-1) downto 0)
+    );
 end entity uart_tx_feed;
 --------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
 architecture rtl of uart_tx_feed is
-	-- UART TX update FSM state declarations
-	type t_uarttx_feed_state is (ST_UARTFEED_IDLE, ST_UARTFEED_CAPT,
-			ST_UARTFEED_DATA, ST_UARTFEED_WAIT);
+    -- UART TX update FSM state declarations
+    type t_uarttx_feed_state is (ST_UARTFEED_IDLE, ST_UARTFEED_CAPT,
+            ST_UARTFEED_DATA, ST_UARTFEED_WAIT);
 
-	-- UART feed FSM state register
-	signal s_uartfeed_pr_state : t_uarttx_feed_state;
-	signal s_uartfeed_nx_state : t_uarttx_feed_state;
+    -- UART feed FSM state register
+    signal s_uartfeed_pr_state : t_uarttx_feed_state;
+    signal s_uartfeed_nx_state : t_uarttx_feed_state;
 
-	-- UART feed FSM auxliary registers
-	signal s_uart_k_val    : natural range 0 to 63;
-	signal s_uart_k_aux    : natural range 0 to 63;
-	signal s_uart_line_val : std_logic_vector((parm_ascii_line_length*8-1) downto 0);
-	signal s_uart_line_aux : std_logic_vector((parm_ascii_line_length*8-1) downto 0);
+    -- UART feed FSM auxliary registers
+    signal s_uart_k_val    : natural range 0 to 63;
+    signal s_uart_k_aux    : natural range 0 to 63;
+    signal s_uart_line_val : std_logic_vector((parm_ascii_line_length*8-1) downto 0);
+    signal s_uart_line_aux : std_logic_vector((parm_ascii_line_length*8-1) downto 0);
 
-	-- preset values on START
-	constant c_uart_k_preset : natural := parm_ascii_line_length;
+    -- preset values on START
+    constant c_uart_k_preset : natural := parm_ascii_line_length;
 
-	-- preset values on reset
-	constant c_line_of_spaces : std_logic_vector((parm_ascii_line_length*8-1) downto 0) :=
-		x"2020202020202020202020202020202020202020202020202020202020202020200D0A";
+    -- preset values on reset
+    constant c_line_of_spaces : std_logic_vector((parm_ascii_line_length*8-1) downto 0) :=
+        x"2020202020202020202020202020202020202020202020202020202020202020200D0A";
 
 begin
-	-- UART TX machine, the \ref parm_ascii_line_length bytes
-	-- of \ref i_dat_ascii_line
-	-- are feed into out the \ref o_tx_data and \ref o_tx_valid signals.
-	-- Another module receives the bytes, indicates readiness on signal
-	-- \ref i_tx_ready .
+    -- UART TX machine, the \ref parm_ascii_line_length bytes
+    -- of \ref i_dat_ascii_line
+    -- are feed into out the \ref o_tx_data and \ref o_tx_valid signals.
+    -- Another module receives the bytes, indicates readiness on signal
+    -- \ref i_tx_ready .
 
-	-- UART TX machine, synchronous state, auxiliary counting register K,
-	-- and auxiliary line data register LINE.
-	p_uartfeed_fsm_state_aux : process(i_clk_40mhz)
-	begin
-		if rising_edge(i_clk_40mhz) then
-			if (i_rst_40mhz = '1') then
-				s_uartfeed_pr_state <= ST_UARTFEED_IDLE;
-				s_uart_k_aux        <= 0;
-				s_uart_line_aux     <= c_line_of_spaces;
-			else
-				s_uartfeed_pr_state <= s_uartfeed_nx_state;
-				s_uart_k_aux        <= s_uart_k_val;
-				s_uart_line_aux     <= s_uart_line_val;
-			end if;
-		end if;
-	end process p_uartfeed_fsm_state_aux;
+    -- UART TX machine, synchronous state, auxiliary counting register K,
+    -- and auxiliary line data register LINE.
+    p_uartfeed_fsm_state_aux : process(i_clk_40mhz)
+    begin
+        if rising_edge(i_clk_40mhz) then
+            if (i_rst_40mhz = '1') then
+                s_uartfeed_pr_state <= ST_UARTFEED_IDLE;
+                s_uart_k_aux        <= 0;
+                s_uart_line_aux     <= c_line_of_spaces;
+            else
+                s_uartfeed_pr_state <= s_uartfeed_nx_state;
+                s_uart_k_aux        <= s_uart_k_val;
+                s_uart_line_aux     <= s_uart_line_val;
+            end if;
+        end if;
+    end process p_uartfeed_fsm_state_aux;
 
-	-- UART TX machine, combinatorial next state, with usage of auxiliary
-	-- counting register and auxiliary text line register.
-	p_uartfeed_fsm_nx_out : process(s_uartfeed_pr_state, s_uart_k_aux, s_uart_line_aux,
-			i_tx_go, i_dat_ascii_line, i_tx_ready)
-	begin
-		case (s_uartfeed_pr_state) is
-			when ST_UARTFEED_CAPT =>
-				-- Capture the input ASCII line and the index K.
-				-- The value of \ref i_tx_ready is also checked as to
-				-- not overflow the UART TX buffer. Once TX is ready,
-				-- begin the enqueue of outgoing data. TX Ready is presumed to
-				-- indicate that the TX FIFO is below the threshold of almost
-				-- full and that enqueueing the full line will not overflow
-				-- the TX FIFO.
-				o_tx_data       <= x"00";
-				o_tx_valid      <= '0';
-				s_uart_k_val    <= c_uart_k_preset;
-				s_uart_line_val <= i_dat_ascii_line;
+    -- UART TX machine, combinatorial next state, with usage of auxiliary
+    -- counting register and auxiliary text line register.
+    p_uartfeed_fsm_nx_out : process(s_uartfeed_pr_state, s_uart_k_aux, s_uart_line_aux,
+            i_tx_go, i_dat_ascii_line, i_tx_ready)
+    begin
+        case (s_uartfeed_pr_state) is
+            when ST_UARTFEED_CAPT =>
+                -- Capture the input ASCII line and the index K.
+                -- The value of \ref i_tx_ready is also checked as to
+                -- not overflow the UART TX buffer. Once TX is ready,
+                -- begin the enqueue of outgoing data. TX Ready is presumed to
+                -- indicate that the TX FIFO is below the threshold of almost
+                -- full and that enqueueing the full line will not overflow
+                -- the TX FIFO.
+                o_tx_data       <= x"00";
+                o_tx_valid      <= '0';
+                s_uart_k_val    <= c_uart_k_preset;
+                s_uart_line_val <= i_dat_ascii_line;
 
-				if (i_tx_ready = '1') then
-					s_uartfeed_nx_state <= ST_UARTFEED_DATA;
-				else
-					s_uartfeed_nx_state <= ST_UARTFEED_CAPT;
-				end if;
+                if (i_tx_ready = '1') then
+                    s_uartfeed_nx_state <= ST_UARTFEED_DATA;
+                else
+                    s_uartfeed_nx_state <= ST_UARTFEED_CAPT;
+                end if;
 
-			when ST_UARTFEED_DATA =>
-				-- Enqueue the \ref c_uart_k_preset count of bytes from register
-				-- \ref s_uart_line_aux. Then transition to the WAIT state.
-				-- To accomplish this, s_uart_line_aux is shifted left, one byte
-				-- at-a-time.
-				o_tx_data       <= s_uart_line_aux(((8 * c_uart_k_preset) - 1) downto (8 * (c_uart_k_preset - 1)));
-				o_tx_valid      <= '1';
-				s_uart_k_val    <= s_uart_k_aux - 1;
-				s_uart_line_val <= s_uart_line_aux((8 * (c_uart_k_preset - 1) - 1) downto 0) & x"00";
+            when ST_UARTFEED_DATA =>
+                -- Enqueue the \ref c_uart_k_preset count of bytes from register
+                -- \ref s_uart_line_aux. Then transition to the WAIT state.
+                -- To accomplish this, s_uart_line_aux is shifted left, one byte
+                -- at-a-time.
+                o_tx_data       <= s_uart_line_aux(((8 * c_uart_k_preset) - 1) downto (8 * (c_uart_k_preset - 1)));
+                o_tx_valid      <= '1';
+                s_uart_k_val    <= s_uart_k_aux - 1;
+                s_uart_line_val <= s_uart_line_aux((8 * (c_uart_k_preset - 1) - 1) downto 0) & x"00";
 
-				if (s_uart_k_aux = 1) then
-					s_uartfeed_nx_state <= ST_UARTFEED_WAIT;
-				else
-					s_uartfeed_nx_state <= ST_UARTFEED_DATA;
-				end if;
+                if (s_uart_k_aux = 1) then
+                    s_uartfeed_nx_state <= ST_UARTFEED_WAIT;
+                else
+                    s_uartfeed_nx_state <= ST_UARTFEED_DATA;
+                end if;
 
-			when ST_UARTFEED_WAIT =>
-				-- Wait for the \ref i_tx_go pulse to be low, and then
-				-- transition to the IDLE state.
-				o_tx_data       <= x"00";
-				o_tx_valid      <= '0';
-				s_uart_k_val    <= s_uart_k_aux;
-				s_uart_line_val <= s_uart_line_aux;
+            when ST_UARTFEED_WAIT =>
+                -- Wait for the \ref i_tx_go pulse to be low, and then
+                -- transition to the IDLE state.
+                o_tx_data       <= x"00";
+                o_tx_valid      <= '0';
+                s_uart_k_val    <= s_uart_k_aux;
+                s_uart_line_val <= s_uart_line_aux;
 
-				if (i_tx_go = '0') then
-					s_uartfeed_nx_state <= ST_UARTFEED_IDLE;
-				else
-					s_uartfeed_nx_state <= ST_UARTFEED_WAIT;
-				end if;
+                if (i_tx_go = '0') then
+                    s_uartfeed_nx_state <= ST_UARTFEED_IDLE;
+                else
+                    s_uartfeed_nx_state <= ST_UARTFEED_WAIT;
+                end if;
 
-			when others => -- ST_UARTFEED_IDLE
-				           -- IDLE the FSM while waiting for a pulse on \ref i_tx_go 
-				o_tx_data       <= x"00";
-				o_tx_valid      <= '0';
-				s_uart_k_val    <= s_uart_k_aux;
-				s_uart_line_val <= s_uart_line_aux;
+            when others => -- ST_UARTFEED_IDLE
+                -- IDLE the FSM while waiting for a pulse on \ref i_tx_go 
+                o_tx_data       <= x"00";
+                o_tx_valid      <= '0';
+                s_uart_k_val    <= s_uart_k_aux;
+                s_uart_line_val <= s_uart_line_aux;
 
-				if (i_tx_go = '1') then
-					s_uartfeed_nx_state <= ST_UARTFEED_CAPT;
-				else
-					s_uartfeed_nx_state <= ST_UARTFEED_IDLE;
-				end if;
-		end case;
-	end process p_uartfeed_fsm_nx_out;
+                if (i_tx_go = '1') then
+                    s_uartfeed_nx_state <= ST_UARTFEED_CAPT;
+                else
+                    s_uartfeed_nx_state <= ST_UARTFEED_IDLE;
+                end if;
+        end case;
+    end process p_uartfeed_fsm_nx_out;
 
 end architecture rtl;
 --------------------------------------------------------------------------------
