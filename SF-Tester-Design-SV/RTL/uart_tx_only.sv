25d1b3cf0dd770e8a7712087eb0d8db2f9a97812
Timothy Stotts <timothystotts08@gmail.com>
2023-02-24 20:52:22 -0500

    Coding style, typos, comments, whitespace were updated in entity and module uart_tx_only .

--- SF-Tester-Design-SV/RTL/uart_tx_only.sv
+++ SF-Tester-Design-SV/RTL/uart_tx_only.sv
@@ -1,7 +1,7 @@
 /*------------------------------------------------------------------------------
 -- MIT License
 --
--- Copyright (c) 2020-2022 Timothy Stotts
+-- Copyright (c) 2020-2023 Timothy Stotts
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,7 @@
 --
 -- \brief A simplified UART function to drive TX characters on a UART board
 --        connection, independent of any RX function (presumed to be ingored).
---        Maximum baudrate is 115200; input clock is 7.37 MHz to support divison
+--        Maximum baudrate is 115200; input clock is 7.37 MHz to support division
 --        to modem clock rates.
 ------------------------------------------------------------------------------*/
 //------------------------------------------------------------------------------
@@ -34,40 +34,40 @@
 //Recursive Moore Machine-------------------------------------------------------
 //Part 1: Module header:--------------------------------------------------------
 module uart_tx_only
-	#(parameter
-		// the Modem Baud Rate of the UART TX machine, max 115200
-		integer parm_BAUD = 115200,
-		// the ASCII line length
-		integer parm_ascii_line_length = 35,
-		// the almost full threshold for FIFO byte count range 0 to 2047
-		logic [10:0] parm_almost_full_thresh = {3'b111,8'hDC}
-		)
-	(
-		// system clock
-		input logic i_clk_40mhz,
-		input logic i_rst_40mhz,
-		// modem clock from MMCM divided down
-		input logic i_clk_7_37mhz,
-		input logic i_rst_7_37mhz,
-		// the output to connect to USB-UART RXD pin
-		output logic eo_uart_tx,
-		// data to transmit out the UART
-		input logic [7:0] i_tx_data,
-		input logic i_tx_valid,
-		// indication that the FIFO is not almost full and can receive a line of data
-		output logic o_tx_ready
-		);
+    #(parameter
+        // the Modem Baud Rate of the UART TX machine, max 115200
+        integer parm_BAUD = 115200,
+        // the ASCII line length
+        integer parm_ascii_line_length = 35,
+        // the almost full threshold for FIFO byte count range 0 to 2047
+        logic [10:0] parm_almost_full_thresh = {3'b111,8'hDC}
+        )
+    (
+        // system clock
+        input logic i_clk_40mhz,
+        input logic i_rst_40mhz,
+        // modem clock from MMCM divided down
+        input logic i_clk_7_37mhz,
+        input logic i_rst_7_37mhz,
+        // the output to connect to USB-UART RXD pin
+        output logic eo_uart_tx,
+        // data to enqueue to a FIFO for tranmission out the UART
+        input logic [7:0] i_tx_data,
+        input logic i_tx_valid,
+        // indication that the FIFO is not almost full and can receive a line of data
+        output logic o_tx_ready
+        );
 
 
 //Part 2: Declarations----------------------------------------------------------
 timeunit 1ns;
 timeprecision 1ps;
 
-/* State Machine constants and variables */
+// State Machine constants and variables
 `define c_uarttxonly_fsm_state_bits 2
 
 typedef enum logic [(`c_uarttxonly_fsm_state_bits - 1):0] {
-	ST_IDLE, ST_START, ST_DATA, ST_STOP
+    ST_IDLE, ST_START, ST_DATA, ST_STOP
 } t_uarttxonly_state;
 
 // Xilinx state machine register attributes.
@@ -77,17 +77,18 @@ typedef enum logic [(`c_uarttxonly_fsm_state_bits - 1):0] {
 t_uarttxonly_state s_uarttxonly_pr_state;
 t_uarttxonly_state s_uarttxonly_nx_state;
 
-/* State machine output and auxiliary registers */
+// State machine output and auxiliary registers
 logic so_uart_tx;
 logic [3:0] s_i_val;
 logic [3:0] s_i_aux;
 logic [7:0] s_data_val;
 logic [7:0] s_data_aux;
 
-/* internal clock for 1x the baud rate */
+// internal clock enable for 1x the baud rate
 logic s_ce_baud_1x;
 
-/* Mapping for FIFO TX */
+// Mapping for FIFO TX. Not all signals are connected outside of the FIFO, but
+// the port map is complete for purpose of viewing on a simulation waveform.
 logic [7:0] s_data_fifo_tx_in;
 logic [7:0] s_data_fifo_tx_out;
 logic s_data_fifo_tx_re;
@@ -106,22 +107,23 @@ logic s_data_fifo_tx_wr_err;
 
 // clock enable for 1x times the baud rate: no oversampling for TX ONLY
 clock_enable_divider #(.par_ce_divisor(4 * 16 * 115200 / parm_BAUD))
-	u_baud_1x_ce_divider (
-	.o_ce_div(s_ce_baud_1x),
-	.i_clk_mhz(i_clk_7_37mhz),
-	.i_rst_mhz(i_rst_7_37mhz),
-	.i_ce_mhz(1'b1));
+    u_baud_1x_ce_divider (
+    .o_ce_div(s_ce_baud_1x),
+    .i_clk_mhz(i_clk_7_37mhz),
+    .i_rst_mhz(i_rst_7_37mhz),
+    .i_ce_mhz(1'b1));
 
-/* FIFO to receive from system and gradually transmit to UART. 
-   The FIFO must implement read-ahead output on rd_en. */
+// FIFO to receive from system and gradually transmit to UART.
+// The FIFO must implement read-ahead output on rd_en.
 assign s_data_fifo_tx_in = i_tx_data;
 assign s_data_fifo_tx_we = i_tx_valid;
 assign o_tx_ready = ((! s_data_fifo_tx_full) && (! s_data_fifo_tx_almostfull));
 
 /*
+// Generate a Valid pulse on TX read
 always_ff @(posedge i_clk_7_37mhz)
 begin: p_gen_fifo_tx_valid
-	s_data_fifo_tx_valid <= s_data_fifo_tx_re;
+    s_data_fifo_tx_valid <= s_data_fifo_tx_re;
 end : p_gen_fifo_tx_valid
 */
 
@@ -147,9 +149,9 @@ FIFO_DUALCLOCK_MACRO  #(
   .ALMOST_EMPTY_OFFSET(11'h023),                 // Sets the almost empty threshold
   .ALMOST_FULL_OFFSET(parm_almost_full_thresh),  // Sets almost full threshold
   .DATA_WIDTH(8),                                // Valid values are 1-72 (37-72 only valid when FIFO_SIZE="36Kb")
-  .DEVICE("7SERIES"),                            // Target device: "7SERIES" 
-  .FIFO_SIZE ("18Kb"),                           // Target BRAM: "18Kb" or "36Kb" 
-  .FIRST_WORD_FALL_THROUGH ("TRUE")              // Sets the FIFO FWFT to "TRUE" or "FALSE" 
+  .DEVICE("7SERIES"),                            // Target device: "7SERIES"
+  .FIFO_SIZE ("18Kb"),                           // Target BRAM: "18Kb" or "36Kb"
+  .FIRST_WORD_FALL_THROUGH ("TRUE")              // Sets the FIFO FWFT to "TRUE" or "FALSE"
 ) u_fifo_uart_tx_0 (
   .ALMOSTEMPTY(s_data_fifo_tx_almostempty), // 1-bit output almost empty
   .ALMOSTFULL(s_data_fifo_tx_almostfull),   // 1-bit output almost full
@@ -168,88 +170,93 @@ FIFO_DUALCLOCK_MACRO  #(
   .WREN(s_data_fifo_tx_we)                  // 1-bit input write enable
 );
 
-// End of FIFO_DUALCLOCK_MACRO_inst instantiation
-				
+// End of u_fifo_uart_tx_0 instantiation
 
-/* FSM register and auxiliary registers */
+
+// FSM register and auxiliary registers
 always_ff @(posedge i_clk_7_37mhz)
 begin: p_uarttxonly_fsm_state_aux
-	if (i_rst_7_37mhz) begin
-		s_uarttxonly_pr_state <= ST_IDLE;
+    if (i_rst_7_37mhz) begin
+        s_uarttxonly_pr_state <= ST_IDLE;
 
-		s_i_aux <= 0;
-		s_data_aux <= 8'h00;
-	end
-	else if (s_ce_baud_1x) begin : if_fsm_state_and_storage
-		s_uarttxonly_pr_state <= s_uarttxonly_nx_state;
+        s_i_aux <= 0;
+        s_data_aux <= 8'h00;
+    end
+    else begin
+        if (s_ce_baud_1x) begin : if_fsm_state_and_storage
+            s_uarttxonly_pr_state <= s_uarttxonly_nx_state;
 
-		s_i_aux <= s_i_val;
-		s_data_aux <= s_data_val;
-	end : if_fsm_state_and_storage
+            s_i_aux <= s_i_val;
+            s_data_aux <= s_data_val;
+        end : if_fsm_state_and_storage
+    end
 end : p_uarttxonly_fsm_state_aux
 
-/* FSM combinatorial logic with output and auxiliary registers */
+// FSM combinatorial logic with output and auxiliary registers
 always_comb
 begin: p_uarttxonly_fsm_nx_out
-	case (s_uarttxonly_pr_state)
-		ST_START: begin
-			/* Transmit the UART serial START bit '0' and load the 
-			   next TX FIFO byte on transition. */
-			s_data_fifo_tx_re = s_ce_baud_1x;
-			s_data_val = s_data_fifo_tx_out;
-			s_i_val = 0;
+    case (s_uarttxonly_pr_state)
+        ST_START: begin
+            // Transmit the UART serial START bit '0' and load the
+            // next TX FIFO byte on transition.
+            s_data_fifo_tx_re = s_ce_baud_1x;
+            s_data_val = s_data_fifo_tx_out;
+            s_i_val = 0;
 
-			so_uart_tx = 1'b0;
+            so_uart_tx = 1'b0;
 
-			s_uarttxonly_nx_state = ST_DATA;
-		end
-		ST_DATA: begin
-			/* Transmit the byte data to UART serial, least significant
-			   bit first, index 0 to 7. */
-			s_data_fifo_tx_re = 1'b0;
-			s_data_val = s_data_aux;
-			s_i_val = s_i_aux + 1;
+            s_uarttxonly_nx_state = ST_DATA;
+        end
+        ST_DATA: begin
+            // Transmit the byte data to UART serial, least significant
+            // bit first, index 0 to 7.
+            s_data_fifo_tx_re = 1'b0;
+            s_data_val = s_data_aux;
+            s_i_val = s_i_aux + 1;
 
-			// Note that it may be more efficient to make this a shift register.
-			so_uart_tx = s_data_aux[s_i_aux];
+            // Note that it may be more efficient to make this a shift register.
+            so_uart_tx = s_data_aux[s_i_aux];
 
-			if (s_i_aux == 7) s_uarttxonly_nx_state = ST_STOP;
-			else s_uarttxonly_nx_state = ST_DATA;
-		end
-		ST_STOP: begin
-			/* Transmit the UART serial STOP bit '1'. Check the FIFO
-			   status. If FIFO contains more data, then transition
-			   directly back to the START bit. Otherwise, transition
-			   to the IDLE state. */
-			s_data_fifo_tx_re = 1'b0;
-			s_data_val = s_data_aux;
-			s_i_val = s_i_aux;
+            if (s_i_aux == 7) s_uarttxonly_nx_state = ST_STOP;
+            else s_uarttxonly_nx_state = ST_DATA;
+        end
+        ST_STOP: begin
+            // Transmit the UART serial STOP bit '1'. Check the FIFO
+            // status. If FIFO contains more data, then transition
+            // directly back to the START bit. This prevents breaks of time
+            // between sending bytes, when there is already more data to
+            // send. Otherwise, transition to the IDLE state.
+            s_data_fifo_tx_re = 1'b0;
+            s_data_val = s_data_aux;
+            s_i_val = s_i_aux;
 
-			so_uart_tx = 1'b1;
+            so_uart_tx = 1'b1;
 
-			if (! s_data_fifo_tx_empty) s_uarttxonly_nx_state = ST_START;
-			else s_uarttxonly_nx_state = ST_IDLE;
-		end
-		default: begin // ST_IDLE
-			/* The IDLE state holds a continuous high value on the
-			   serial line to indicate UART signal is IDLE. */
-			s_data_fifo_tx_re = 1'b0;
-			s_data_val = s_data_aux;
-			s_i_val = s_i_aux;
+            if (! s_data_fifo_tx_empty) s_uarttxonly_nx_state = ST_START;
+            else s_uarttxonly_nx_state = ST_IDLE;
+        end
+        default: begin // ST_IDLE
+            // The IDLE state holds a continuous high value on the
+            // serial line to indicate UART signal is IDLE.
+            s_data_fifo_tx_re = 1'b0;
+            s_data_val = s_data_aux;
+            s_i_val = s_i_aux;
 
-			so_uart_tx = 1'b1;
+            so_uart_tx = 1'b1;
 
-			if (! s_data_fifo_tx_empty) s_uarttxonly_nx_state = ST_START;
-			else s_uarttxonly_nx_state = ST_IDLE;
-		end
-	endcase
+            if (! s_data_fifo_tx_empty) s_uarttxonly_nx_state = ST_START;
+            else s_uarttxonly_nx_state = ST_IDLE;
+        end
+    endcase
 end : p_uarttxonly_fsm_nx_out
 
-/* Registered output for timing closure and glitch removal on the output pin */
+// Registered output for timing closure and glitch removal on the output pin
 always_ff @(posedge i_clk_7_37mhz)
 begin: p_fsm_out_reg
-	if (i_rst_7_37mhz) eo_uart_tx <= 1'b1;
-	else if (s_ce_baud_1x) eo_uart_tx <= so_uart_tx;
+    if (i_rst_7_37mhz) eo_uart_tx <= 1'b1; // line IDLE
+    else begin
+        if (s_ce_baud_1x) eo_uart_tx <= so_uart_tx; // registered output
+    end
 end : p_fsm_out_reg
 
 endmodule : uart_tx_only
